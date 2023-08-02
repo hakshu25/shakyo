6a581cff453eeaebec07d200ddbe32f9d0a1484e
hakshu <hakshu25@gmail.com>
2023-08-02 16:33:58 +0900

    feat: ハンズオン用の基本的なファイルを作成(第2章)

---
+++ frontend-security/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => {
+  res.end('Top page');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
