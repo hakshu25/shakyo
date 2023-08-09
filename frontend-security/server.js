5bcd761fd39a9233cbddd04fd4c4e419034f4505
hakshu <hakshu25@gmail.com>
2023-08-09 17:28:17 +0900

    feat: 5章のハンズオン実装を追加

--- frontend-security/server.js
+++ frontend-security/server.js
@@ -1,14 +1,25 @@
 import express from 'express';
+import crypto from 'crypto';
 import api from './routes/api.js';
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/api', api);
 
 app.get('/', (req, res, next) => {
   res.end('Top page');
+});
+
+app.get('/csp', (req, res, next) => {
+  const nonceValue = crypto.randomBytes(16).toString('base64');
+  res.header(
+    'Content-Security-Policy',
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic'; object-src 'none'; base-uri 'none'; require-trusted-types-for 'script';`
+  );
+  res.render('csp', { nonce: nonceValue });
 });
 
 app.listen(port, () => {
